562eb572e9af2d1e12f56e0a51e4f206c366cb12
Muhammad-Muzzamal <ashrafmuzzamal7@gmail.com>
2025-11-14 22:00:05 +0500

    jkb

--- app.js
+++ app.js
@@ -4,12 +4,15 @@ const mongoose = require("mongoose");
 const Listing = require("./models/listings.model");
 const path = require("path");
 const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
 
 const app = express();
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
+app.engine("ejs", ejsMate);
+app.use(express.static(path.join(__dirname, "/public")));
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
@@ -35,17 +38,15 @@ app.get("/listings", async (req, res) => {
   return res.render("listings/index.ejs", { allListing });
 });
 // Show route
+app.get("/listings/new", (req, res) => {
+  res.render("listings/new.ejs");
+});
+// Show route
 app.get("/listings/:id", async (req, res) => {
   let { id } = req.params;
   const listing = await Listing.findById(id);
   return res.render("listings/show.ejs", { listing });
 });
-
-// Show route
-app.get("/listing/new", (req, res) => {
-  res.render("listings/new.ejs");
-});
-
 // Create Listing route
 app.post("/listings", async (req, res) => {
   const { title, description, location, image, price } = req.body;
